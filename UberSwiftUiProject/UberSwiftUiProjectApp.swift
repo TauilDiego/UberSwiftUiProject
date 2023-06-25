15918b3a45790479bac40f88d258f8f05e49d13e
Diego macbook (m1) <tauildiego@icloud.com>
2023-06-24 21:43:38 -0300

    Initial Commit

---
+++ UberSwiftUiProject/UberSwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUiProjectApp.swift
+//  UberSwiftUiProject
+//
+//  Created by Diego Tauil on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
